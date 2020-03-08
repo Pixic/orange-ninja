aab97923e8932ea14fed882a6298605ce4070d63
Pixic <mattias.isene@gmail.com>
2020-03-08 21:23:11 +0100

    Updates

---
+++ precache-manifest.e2ef195ade86e171759920d58962bb86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1da7514416ef316b19c2fe73a5935a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b06c613fdae6d6ff25",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "10d8bc04e4d876eb83ab",
+    "url": "/static/js/2.8ceb484d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8ceb484d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b06c613fdae6d6ff25",
+    "url": "/static/js/main.a4e6b96f.chunk.js"
+  },
+  {
+    "revision": "20db05fbf0045b6ba860",
+    "url": "/static/js/runtime-main.ac740d55.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
